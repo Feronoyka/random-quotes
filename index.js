ba2ee787f6aa21c4f6d931c36adeaecf9ec7eed9
Feronoyka <feronoyka@gmail.com>
2025-03-19 21:58:07 +0500

    added quote and author separatly, changed array quotes to object quotes

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const quotes = ['Howman', 'Stiven', 'Albert', 'Richard'];
+const quotes = [
+  {
+    quote: 'Stay hungry, stay foolish',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'To be or not to be, that is the question',
+    author: 'William Shakespeare',
+  },
+  {
+    quote: 'To err is human; to forgive, divine',
+    author: 'Alexandr Rope',
+  },
+];
 
-const quote = document.getElementById('quote');
+const quoteElement = document.getElementById('quote');
+const authorElement = document.getElementById('author');
 const generateBtn = document.getElementById('generate-btn');
 
 const generateRandomQuotes = () => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomElement = quotes[randomIndex];
-  quote.textContent = randomElement;
+  const randomQuote = quotes[randomIndex];
+  // const quoteElement =
+  //   '<em>"' + randomQuote.quote + '"</em>' + '<br>' + randomQuote.author;
+  // const quote = randomQuote.quote;
+  // const author = `(${randomQuote.author})`;
+  // const { quote, author } = randomquote;
+  quoteElement.textContent = randomQuote.quote;
+  authorElement.textContent = randomQuote.author;
 };
 
 generateBtn.addEventListener('click', generateRandomQuotes);
