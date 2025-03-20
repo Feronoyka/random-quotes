d46c1381d99098bf81dac2bd3e1047de5aeb09e0
Feronoyka <feronoyka@gmail.com>
2025-03-20 18:20:46 +0500

    splitted array into other file

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const quotes = [
-  {
-    quote: 'Stay hungry, stay foolish',
-    author: 'Steve Jobs',
-  },
-  {
-    quote: 'To be or not to be, that is the question',
-    author: 'William Shakespeare',
-  },
-  {
-    quote: 'To err is human; to forgive, divine',
-    author: 'Alexandr Rope',
-  },
-];
+import quotes from './quotes.js';
 
 const quoteElement = document.getElementById('quote');
 const authorElement = document.getElementById('author');
---
+++ quotes.js
@@ -0,0 +1,16 @@
+const quotes = [
+  {
+    quote: 'Stay hungry, stay foolish',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'To be or not to be, that is the question',
+    author: 'William Shakespeare',
+  },
+  {
+    quote: 'To err is human; to forgive, divine',
+    author: 'Alexandr Rope',
+  },
+];
+
+export default quotes;
