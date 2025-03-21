d55ed3324fffb69bf0c30c4b0d6fd98437d45c0e
Feronoyka <feronoyka@gmail.com>
2025-03-21 11:08:38 +0500

    Show favourite quotes below current quote

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import quotes from './quotes.js';
 const quoteElement = document.getElementById('quote');
 const authorElement = document.getElementById('author');
 const generateBtn = document.getElementById('generate-btn');
+const toggleFavouriteBtn = document.getElementById('toggle-favourite');
+const favouritesContainer = document.getElementById('favourites-container');
+
+let currentQuoteIndex;
 
 const generateRandomQuotes = () => {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   // const quoteElement =
   //   '<em>"' + randomQuote.quote + '"</em>' + '<br>' + randomQuote.author;
   // const quote = randomQuote.quote;
@@ -14,6 +18,37 @@ const generateRandomQuotes = () => {
   // const { quote, author } = randomquote;
   quoteElement.textContent = randomQuote.quote;
   authorElement.textContent = randomQuote.author;
+  toggleFavouriteBtn.textContent = randomQuote.isFavourite
+    ? 'Remove from favourite'
+    : 'Add to favourite';
+
+  toggleFavouriteBtn.style.display = 'inline-block';
 };
 
+function toggleFovourite() {
+  const currentQuotes = quotes[currentQuoteIndex];
+  currentQuotes.isFavourite = !currentQuotes.isFavourite;
+  toggleFavouriteBtn.textContent = currentQuotes.isFavourite
+    ? 'Remove from favourite'
+    : 'Add to favourite';
+
+  if (currentQuotes.isFavourite) {
+    const favouriteCard = document.createElement('div');
+    favouriteCard.classList.add('favourite-card');
+    favouriteCard.innerHTML = `
+    <p>${currentQuotes.quote}</p>
+    <p class="author">${currentQuotes.author}</p>
+    `;
+    favouritesContainer.appendChild(favouriteCard);
+  } else {
+    const favouriteCards = document.querySelectorAll('.favourite-card');
+    favouriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuotes.quote)) {
+        card.remove();
+      }
+    });
+  }
+}
+
 generateBtn.addEventListener('click', generateRandomQuotes);
+toggleFavouriteBtn.addEventListener('click', toggleFovourite);
--- quotes.js
+++ quotes.js
@@ -2,14 +2,17 @@ const quotes = [
   {
     quote: 'Stay hungry, stay foolish',
     author: 'Steve Jobs',
+    isFavourite: false,
   },
   {
     quote: 'To be or not to be, that is the question',
     author: 'William Shakespeare',
+    isFavourite: false,
   },
   {
     quote: 'To err is human; to forgive, divine',
     author: 'Alexandr Rope',
+    isFavourite: false,
   },
 ];
 
